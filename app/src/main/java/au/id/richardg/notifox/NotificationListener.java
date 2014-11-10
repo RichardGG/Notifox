be649f6c3516983d442bc0410a73d2890acf8f9a
Richard Gilbert <richardggilbert@gmail.com>
2014-11-10 22:09:15 +1000

    Added PebbleKit

--- app/src/main/java/au/id/richardg/notifox/NotificationListener.java
+++ app/src/main/java/au/id/richardg/notifox/NotificationListener.java
@@ -1,8 +1,11 @@
 package au.id.richardg.notifox;
 
+import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
+
+import com.getpebble.android.kit.PebbleKit;
 
 /**
  * Created by Richard on 10/11/2014.
@@ -10,9 +13,19 @@ import android.util.Log;
 public class NotificationListener extends NotificationListenerService{
 
     @Override
-    public  void onNotificationPosted(StatusBarNotification sbn) {
-        Log.d("new notif", "woo");
+    public void  onCreate() {
 
+    }
+
+
+    @Override
+    public  void onNotificationPosted(StatusBarNotification sbn) {
+        Log.d("1new notif", "1woo");
+
+        boolean connected = PebbleKit.isWatchConnected(getApplicationContext());
+        Log.i("test", "Pebble is " + (connected ? "connected" : "not connected"));
+
+        Log.d("done", "done");
     }
 
     @Override
