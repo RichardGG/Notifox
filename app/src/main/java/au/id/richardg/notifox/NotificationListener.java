40c170999a0aef460a263b4eaf6d8090ae44e3dd
Richard Gilbert <richardggilbert@gmail.com>
2014-11-14 18:16:23 +1000

    Testing appmessage

--- app/src/main/java/au/id/richardg/notifox/NotificationListener.java
+++ app/src/main/java/au/id/richardg/notifox/NotificationListener.java
@@ -1,20 +1,45 @@
 package au.id.richardg.notifox;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
 import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
+import java.util.UUID;
 
 /**
  * Created by Richard on 10/11/2014.
  */
 public class NotificationListener extends NotificationListenerService{
 
+    private final static UUID PEBBLE_APP_UUID = UUID.fromString("42a10d1a-fd03-4d98-b296-2737d083b3ad");
+
     @Override
     public void  onCreate() {
 
+        Log.d("NotificationListener", "Created");
+
+        PebbleKit.registerReceivedDataHandler(this, new PebbleKit.PebbleDataReceiver(PEBBLE_APP_UUID) {
+            @Override
+            public void receiveData(Context context, final int transactionId, PebbleDictionary data) {
+
+                Log.d("NotificationListener", "Received");
+
+                Log.d("NotificationListener", "received: " + data.getInteger(0) + " from " + data.toJsonString());
+                //Log.i("weee", "Received value=" + data.getUnsignedIntegerAsLong(0) + " for key: 0");
+
+                PebbleDictionary newData = new PebbleDictionary();
+                newData.addInt32(0,43);
+                PebbleKit.sendDataToPebble(getApplicationContext(),PEBBLE_APP_UUID,newData);
+
+
+                PebbleKit.sendAckToPebble(getApplicationContext(),transactionId);
+            }
+        });
     }
 
 
