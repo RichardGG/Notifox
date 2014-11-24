3083b8eff95bb541f9439dc55b8c55b818588730
Richard Gilbert <richardggilbert@gmail.com>
2014-11-24 21:04:08 +1000

    Added unique ids for notifications

--- app/src/main/java/au/id/richardg/notifox/MessageBuilder.java
+++ app/src/main/java/au/id/richardg/notifox/MessageBuilder.java
@@ -1,18 +1,12 @@
 package au.id.richardg.notifox;
 
-import android.app.Notification;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.service.notification.StatusBarNotification;
-import android.util.DisplayMetrics;
 import android.util.Log;
 
 import com.getpebble.android.kit.util.PebbleDictionary;
 
+import java.util.Map;
 import java.util.UUID;
 
 /*
@@ -27,6 +21,9 @@ public class MessageBuilder {
 
     private MessageInterface mMessageInterface;
     private UUID mPebbleAppUuid;
+
+    private Map<Integer, StatusBarNotification> uniqueNotifications;
+    private static int uniqueIDCounter = 0;
 
     public MessageBuilder(MessageInterface messageInterface, UUID pebbleAppUuid) {
         mMessageInterface = messageInterface;
@@ -45,9 +42,14 @@ public class MessageBuilder {
     public void activeNotificationsRequest(Context context, StatusBarNotification[] activeNotifications, int currentPos){
         Log.i(TAG, "activeNotificationsRequest()");
 
+        //make sure all notifications have unique ids
+        updateUniqueIDs(activeNotifications);
 
-
+        //sort the notifications
         activeNotifications = sortNotifications(activeNotifications);
+
+        //find most suitable position to send back
+
 
         //first notif in range
         int firstPos = currentPos;
@@ -59,21 +61,26 @@ public class MessageBuilder {
         if(lastPos > activeNotifications.length)
             lastPos = activeNotifications.length;
 
-        //ids of range
+        //send ids of range
         int[] ids = new int[4];
         for(int i = firstPos; i < firstPos+4; i++) {
-            ids[0] = getId(activeNotifications[i]);
+            ids[0] = getUniqueID(activeNotifications[i]);
         }
-        sendIds(ids, firstPos, activeNotifications.length);
+        sendIds(context, ids, firstPos, activeNotifications.length);
 
         //send notifs in range
         for(int i = firstPos; i < firstPos+4; i++){
-            sendNotification(activeNotifications[i]);
+            sendNotification(context, activeNotifications[i]);
         }
+    }
 
+    private StatusBarNotification[] sortNotifications(StatusBarNotification[] notifications){
+        //TODO
+        return notifications;
+    }
 
-
-
+    private void sendIds(Context context, int[] ids, int pos, int count){
+        //TODO
 
         PebbleDictionary sizeTest = new PebbleDictionary();
 
@@ -83,26 +90,67 @@ public class MessageBuilder {
         sizeTest.addBytes(0,bytes);
 
         mMessageInterface.send(context, sizeTest);
-
     }
 
-    private int getId(StatusBarNotification sbn){
-        //TODO
-        return 0;
-    }
-
-    private StatusBarNotification[] sortNotifications(StatusBarNotification[] notifications){
-        //TODO
-        return notifications;
-    }
-
-    private void sendIds(int[] ids, int pos, int count)
-    {
+    private void sendNotification(Context context, StatusBarNotification sbn){
         //TODO
     }
 
-    private void sendNotification(StatusBarNotification sbn){
-        //TODO
+    private int getUniqueID(StatusBarNotification sbn){
+
+        //find the unique key
+        int id = -1;
+        boolean found = false;
+        for(Map.Entry<Integer,StatusBarNotification> entry : uniqueNotifications.entrySet()){
+            if(sbn.getPackageName().contentEquals(entry.getValue().getPackageName()) && sbn.getId() == entry.getValue().getId()){
+                found = true;
+                id = entry.getKey();
+                entry.setValue(sbn);
+            }
+        }
+
+        //if not found, create it
+        if(!found){
+            //find a new unique key
+            while(uniqueNotifications.containsKey(uniqueIDCounter))
+                uniqueIDCounter++;
+
+            uniqueNotifications.put(uniqueIDCounter,sbn);
+            id = uniqueIDCounter;
+        }
+
+        return id;
     }
+
+    private void updateUniqueIDs(StatusBarNotification[] notifications){
+
+        //for each notification
+        for(int i = 0; i < notifications.length; i++) {
+            //find or create
+            getUniqueID(notifications[i]);
+        }
+
+        //clean up any removed notifications
+        cleanUniqueIDs(notifications);
+    }
+
+    private void cleanUniqueIDs(StatusBarNotification[] notifications){
+
+        //for all saved notifications
+        for(Map.Entry<Integer,StatusBarNotification> entry : uniqueNotifications.entrySet()){
+            //see if it is active
+            boolean found = false;
+            for(int i = 0; i < notifications.length; i++){
+                if(notifications[i].getPackageName().contentEquals(entry.getValue().getPackageName()) && notifications[i].getId() == entry.getValue().getId()){
+                    found = true;
+                }
+            }
+
+            //otherwise remove it
+            if(!found)
+                uniqueNotifications.remove(entry.getKey());
+        }
+    }
+
 
 }
